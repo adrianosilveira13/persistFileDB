8fd836389ec0c9210c0fd836fa0cac2af5a7f704
Adriano <kdr@outlook.com.br>
2022-03-22 21:07:25 -0300

    ensure save controller returns 400 if no data is provided

--- controllers/saveController.js
+++ controllers/saveController.js
@@ -5,7 +5,13 @@ exports.saveForm = async (req, res, next) => {
   try {
     const dataFile = await fsPromises.readFile(`${__dirname}/../data/data.json`, 'utf-8')
     const data = JSON.parse(dataFile)
-    data.push(req.body)
+    if (!req.body.data) {
+      return res.status(400).json({
+        status: 'fail',
+        error: 'MissingInputData'
+      })
+    }
+    data.push(req.body.data)
     await fsPromises.writeFile(`${__dirname}/../data/data.json`, JSON.stringify(data), 'utf-8')
     res.status(200).json({
       status: 'success'
@@ -13,5 +19,4 @@ exports.saveForm = async (req, res, next) => {
   } catch (e) {
     console.log(e)
   }
-  
 }
